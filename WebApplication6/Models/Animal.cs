3b2875c78fd05aa5ee280977c5dfedc969436d92
Piotr Głuchowski <s17433@pjwstk.edu.pl>
2024-05-26 14:59:43 +0200

    first commit

---
+++ WebApplication6/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models;
+
+public class Animal
+{
+    [Key]
+    public int IdAnimal { get; set; }
+    
+    [StringLength(100)]
+    public string Name { get; set; }
+    
+}
---
+++ WebApplication6/Models/PjatkDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication6.Models;
+
+public class PjatkDbContext: DbContext {
+    
+    
+    public DbSet<Animal> Animals { get; set; }
+}
