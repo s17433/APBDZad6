9e9612cc357b8d338321c46c6ace709c7264af9a
s17433 <162893223+s17433@users.noreply.github.com>
2024-06-04 22:23:16 +0200

    Add files via upload

---
+++ WebApplication6/Services/IPrescriptionService.cs
@@ -0,0 +1,9 @@
+﻿using APBD_06.Model;
+
+namespace APBD_06.Services;
+
+public interface IPrescriptionService
+{
+    int createPerscription(NewPrescriptionRequest newPrescription);
+    PatientInfo GetPatientInfo(int id);
+}
---
+++ WebApplication6/Services/PrescriptionService.cs
@@ -0,0 +1,24 @@
+﻿using APBD_06.Model;
+using APBD_06.Repositories;
+
+namespace APBD_06.Services;
+
+public class PrescriptionService : IPrescriptionService
+{
+    private readonly IPrescriptionRepository _perscriptionRepository;
+    
+    public PrescriptionService(IPrescriptionRepository prescriptionRepository)
+    {
+        _perscriptionRepository = prescriptionRepository;
+    }
+
+    public int createPerscription(NewPrescriptionRequest newPrescription)
+    {
+        return _perscriptionRepository.CreatePrescription(newPrescription);
+    }
+
+    public PatientInfo GetPatientInfo(int id)
+    {
+        return _perscriptionRepository.GetPatientInfo(id);
+    }
+}
